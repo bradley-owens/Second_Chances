447991c8da017d3ce3f08fa5c9220f727370da07
Bradley Owens <bradowens17@hotmail.com>
2022-08-22 12:49:49 +1000

    Add styling to the pet modals

--- src/Components/PetModal/PetModal.js
+++ src/Components/PetModal/PetModal.js
@@ -7,22 +7,40 @@ const PetModal = (props) => {
   console.log(pet);
   return (
     <Modal onClose={props.onCloseModal}>
-      <img src={pet.primary_photo_cropped.small}></img>
-      <h2>{pet.name}</h2>
-      <h3>{pet.breeds.primary}</h3>
-      <p>{pet.size}</p>
-      <p>{pet.age}</p>
-      <p>{`${
-        pet.tags.length <= 0
-          ? pet.status
-          : pet.tags[0] + "/" + pet.tags[1] + "/" + pet.tags[2]
-      }`}</p>
-      <p>{`City: ${pet.contact.address.city} State:${pet.contact.address.state} `}</p>
-      <p>{pet.status}</p>
-      <a href={pet.url}>{pet.url}</a>
-      <p>{pet.contact.phone}</p>
-      <p>{pet.contact.email}</p>
-      <button onClick={props.onCloseModal}>Close</button>
+      <div className={styles["modal-flex"]}>
+        <img
+          className={styles["modal-img"]}
+          src={pet.primary_photo_cropped.small}
+        ></img>
+        <div className={styles["flex-info"]}>
+          <h2 className={styles.name}>{pet.name}</h2>
+          <h3 className={styles.breed}>{pet.breeds.primary}</h3>
+          <p>{pet.size}</p>
+          <p>{pet.age}</p>
+          <p>{`${
+            pet.tags.length <= 0
+              ? "No Description"
+              : pet.tags[0] + "/" + pet.tags[1] + "/" + pet.tags[2]
+          }`}</p>
+        </div>
+      </div>
+      <div className={styles["modal-bottom"]}>
+        <div>
+          <p>{pet.status}</p>
+          <p>{`City: ${pet.contact.address.city}`}</p>
+          <p>{`State: ${pet.contact.address.state} `}</p>
+          <p>{pet.contact.phone}</p>
+          <p>{pet.contact.email}</p>
+        </div>
+        <div className={styles.adopt}>
+          <a className={styles.button} href={pet.url} target="_blank">
+            Adopt!
+          </a>
+          <button className={styles.close} onClick={props.onCloseModal}>
+            Close
+          </button>
+        </div>
+      </div>
     </Modal>
   );
 };
